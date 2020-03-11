a8a70f14ecef78efd4d585b9ba5ef09c9e43f9b1
Andrei Gavriliu <andrei@gavriliu.com>
2020-03-11 21:36:03 +0100

    rewrote code

--- ff3_prometheus_exporter.py
+++ ff3_prometheus_exporter.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
-Prometheus Exporter for your Firefly-III installation
+Prometheus exporter for Firefly-III
+Version: v0.2
+API Documentation: https://api-docs.firefly-iii.org
 """
-import os
-import sys
+
 import time
 import json
+import os
+import sys
 from datetime import datetime
-from calendar import monthrange
 import requests
-from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Counter, Info
 
 # check and set installation BaseURL
 if (not 'FF3_EXPORTER_BASEURL' in os.environ) or (not os.environ['FF3_EXPORTER_BASEURL']):
@@ -37,243 +41,182 @@ if (not 'FF3_EXPORTER_SLEEP' in os.environ) or (not os.environ['FF3_EXPORTER_SLE
 else:
     FF3_EXPORTER_SLEEP = int(os.environ['FF3_EXPORTER_SLEEP'])
 
-CURRENT_DAY = datetime.today().strftime("%Y-%m-%d")
-CURRENT_MONTH_START = datetime.today().strftime("%Y-%m") + '-01'
-CURRENT_MONTH_END = monthrange(
-    int(datetime.today().strftime("%Y")),
-    int(datetime.today().strftime("%-m")))[1]
+# initialize metrics
+CLIENTS_METRICS = {
+    'ff3_piggybanks': Counter(
+        'ff3_piggybanks',
+        'Total piggybanks count',
+        ['baseurl']),
+    'ff3_piggybank_current_amount': Counter(
+        'ff3_piggybank_current_amount',
+        'Piggybank current amount',
+        ['baseurl', 'piggybank_id', 'piggybank_name']),
+    'ff3_piggybank_target_amount': Counter(
+        'ff3_piggybank_target_amount',
+        'Piggybank target amount',
+        ['baseurl', 'piggybank_id', 'piggybank_name']),
+    'ff3_accounts': Counter(
+        'ff3_accounts',
+        'Total accounts count',
+        ['baseurl']),
+    'ff3_transactions_by_account': Counter(
+        'ff3_transactions_by_account',
+        'Total transactions by account',
+        ['baseurl', 'account_id', 'account_name']),
+    'ff3_transactions': Counter(
+        'ff3_transactions',
+        'Total transaction count',
+        ['baseurl']),
+    'ff3_bills': Counter(
+        'ff3_bills',
+        'Total bills count', ['baseurl']),
+    'ff3': Info(
+        'ff3',
+        'Details about your Firefly-III deployment',
+        ['baseurl'])
+}
 
-class CustomCollector():
+def ff3():
     """
-    standard metrics collection class
+    Collect system information
     """
-    # def __init__(self):
-    #     pass
+    ff3_response = requests.get(
+        '{}/api/v1/about'.format(FF3_EXPORTER_BASEURL),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3(): Response is not JSON format')
 
-    def collect(self):
-        """
-        standard metrics collection function
-        """
-        # get transactions
-        transactions_counter = CounterMetricFamily(
-            'transactions_count',
-            'Total transaction count')
-        transactions_counter.add_metric([
-            'transactions_count'], ff3_transactions()[
-                "meta"][
-                    "pagination"][
-                        "total"])
-        yield transactions_counter
+def ff3_transactions():
+    """
+    get all transactions from Firefly-III
+    """
+    ff3_transactions_response = requests.get(
+        '{}/api/v1/transactions'.format(FF3_EXPORTER_BASEURL),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_transactions_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_transactions(): Response is not JSON format')
 
-        # get bills
-        bills_counter = CounterMetricFamily(
-            'bills_count',
-            'Total bills count')
-        bills_counter.add_metric([
-            'bills_count'], ff3_bills()[
-                "meta"][
-                    "pagination"][
-                        "total"])
-        yield bills_counter
+def ff3_bills():
+    """
+    get all bills from Firefly-III
+    """
+    ff3_bills_response = requests.get(
+        '{}/api/v1/bills'.format(FF3_EXPORTER_BASEURL),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_bills_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_bills(): Response is not JSON format')
 
-        # get accounts
-        # accounts = ff3_accounts()
-        # accounts = accounts['data']
+def ff3_piggybanks():
+    """
+    get list of piggybanks from Firefly-III
+    """
+    ff3_piggybanks_response = requests.get(
+        '{}/api/v1/piggy_banks'.format(FF3_EXPORTER_BASEURL),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_piggybanks_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_piggybanks(): Response is not JSON format')
 
-        # get account balances
-        account_balance_gauge = GaugeMetricFamily(
-            'account_current_balance',
-            'Current Account Balance',
-            labels=[
-                'account_id',
-                'account_name'])
-        for account in ff3_accounts()['data']:
-            if account['attributes']['type'] == 'asset':
-                account_balance_gauge.add_metric([
-                    account['id'],
-                    account['attributes']['name']], account[
-                        'attributes'][
-                            'current_balance'])
-        yield account_balance_gauge
+def ff3_piggybanks_details(piggybank_id):
+    """
+    get piggybank details from Firefly-III
+    """
+    ff3_piggybanks_details_response = requests.get(
+        '{}/api/v1/piggy_banks/{}'.format(
+            FF3_EXPORTER_BASEURL,
+            piggybank_id),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_piggybanks_details_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_piggybanks_details(): Response is not JSON format')
 
-        # get account transactions count (total)
-        account_transaction_counter = CounterMetricFamily(
-            'account_transaction_count',
-            'Total transaction count for account',
-            labels=[
-                'account_id',
-                'account_name'])
-        for account in ff3_accounts()['data']:
-            if account['attributes']['type'] == 'asset':
-                accounts_transactions = ff3_accounts_transactions(
+def ff3_accounts():
+    """
+    get all accounts from Firefly-III
+    """
+    ff3_accounts_response = requests.get(
+        '{}/api/v1/accounts?type=asset'.format(FF3_EXPORTER_BASEURL),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_accounts_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_accounts(): Response is not JSON format')
+
+def ff3_transactions_by_account(account, start='%7B%7D', end='%7B%7D'):
+    """
+    get all account transactions
+    """
+    ff3_transactions_by_account_response = requests.get(
+        '{}/api/v1/accounts/{}/transactions?start={}&end={}'.format(
+            FF3_EXPORTER_BASEURL,
+            account,
+            start,
+            end),
+        headers=json.loads(FF3_EXPORTER_TOKEN))
+    try:
+        return ff3_transactions_by_account_response.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit('ff3_transactions_by_account(): Response is not JSON format')
+
+def collect():
+    """
+    Main function to export collected  metrics
+    """
+    CLIENTS_METRICS['ff3'].labels(FF3_EXPORTER_BASEURL).info({
+        'version': ff3()['data']['version'],
+        'api_version': ff3()['data']['version'],
+        'php_version': ff3()['data']['php_version'],
+        'os': ff3()['data']['os']})
+
+    CLIENTS_METRICS['ff3_transactions'].labels(FF3_EXPORTER_BASEURL).inc(
+        ff3_transactions()['meta']['pagination']['total'])
+
+    CLIENTS_METRICS['ff3_bills'].labels(FF3_EXPORTER_BASEURL).inc(
+        ff3_bills()['meta']['pagination']['total'])
+
+    CLIENTS_METRICS['ff3_accounts'].labels(FF3_EXPORTER_BASEURL).inc(
+        ff3_accounts()['meta']['pagination']['total'])
+
+    CLIENTS_METRICS['ff3_piggybanks'].labels(FF3_EXPORTER_BASEURL).inc(
+        ff3_piggybanks()['meta']['pagination']['total'])
+
+    for account in ff3_accounts()['data']:
+        CLIENTS_METRICS['ff3_transactions_by_account'].labels(
+            FF3_EXPORTER_BASEURL,
+            account['id'],
+            account['attributes']['name']).inc(
+                ff3_transactions_by_account(
                     account=account['id'],
                     start='',
-                    end='')
-                account_transaction_counter.add_metric([
-                    account['id'],
-                    account['attributes']['name']], accounts_transactions[
-                        'meta'][
-                            'pagination'][
-                                'total'])
-        yield account_transaction_counter
+                    end='')['meta']['pagination']['total'])
 
-        # get account transactions count (today)
-        account_transaction_today_counter = CounterMetricFamily(
-            'account_transaction_count_today',
-            'Total transaction count for account today',
-            labels=[
-                'account_id',
-                'account_name'])
-        for account in ff3_accounts()['data']:
-            if account['attributes']['type'] == 'asset':
-                ff3_accounts_transactions_daily = ff3_accounts_transactions(
-                    account=account['id'],
-                    start=CURRENT_DAY,
-                    end=CURRENT_DAY)
-                account_transaction_today_counter.add_metric([
-                    account['id'],
-                    account['attributes']['name']], ff3_accounts_transactions_daily[
-                        'meta'][
-                            'pagination'][
-                                'total'])
-        yield account_transaction_today_counter
+    for piggybank in ff3_piggybanks()['data']:
+        CLIENTS_METRICS['ff3_piggybank_target_amount'].labels(
+            FF3_EXPORTER_BASEURL,
+            piggybank['id'],
+            piggybank['attributes']['name']).inc(
+                ff3_piggybanks_details(
+                    piggybank_id=piggybank['id'])['data']['attributes']['target_amount'])
 
-        # get piggybank target_amount
-        # piggybanks = ff3_piggybanks()
-        # piggybanks = piggybanks['data']
-
-        # get piggybank target amount
-        piggybanks_details_target_amount = CounterMetricFamily(
-            'piggybanks_details_target',
-            'Piggy Bank target amount',
-            labels=[
-                'piggybank_id',
-                'piggybank_name'])
-
-        for piggybank in ff3_piggybanks()['data']:
-            ff3_piggybanks_details_target_amount = ff3_piggybanks_details(
-                piggybank_id=piggybank['id'])
-            piggybanks_details_target_amount.add_metric([
-                piggybank['id'],
-                piggybank['attributes']['name']], ff3_piggybanks_details_target_amount[
-                    'data'][
-                        'attributes'][
-                            'target_amount'])
-        yield piggybanks_details_target_amount
-
-        # get piggybank current amount
-        piggybanks_details_current_amount = CounterMetricFamily(
-            'piggybanks_details_current',
-            'Piggy Bank current amount',
-            labels=[
-                'piggybank_id',
-                'piggybank_name'])
-
-        for piggybank in ff3_piggybanks()['data']:
-            ff3_piggybanks_details_current_amount = ff3_piggybanks_details(
-                piggybank_id=piggybank['id'])
-            piggybanks_details_current_amount.add_metric([
-                piggybank['id'],
-                piggybank['attributes']['name']], ff3_piggybanks_details_current_amount[
-                    'data'][
-                        'attributes'][
-                            'current_amount'])
-        yield piggybanks_details_current_amount
+    for piggybank in ff3_piggybanks()['data']:
+        CLIENTS_METRICS['ff3_piggybank_current_amount'].labels(
+            FF3_EXPORTER_BASEURL,
+            piggybank['id'],
+            piggybank['attributes']['name']).inc(
+                ff3_piggybanks_details(
+                    piggybank_id=piggybank['id'])['data']['attributes']['current_amount'])
 
 if __name__ == '__main__':
-
-    def ff3_transactions():
-        """
-        get all transactions from Firefly-III
-        """
-        transactions = requests.get(
-            '{}/api/v1/transactions'.format(FF3_EXPORTER_BASEURL),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            transactions = transactions.json()
-            return transactions
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_transactions(): Response is not JSON format')
-
-    def ff3_accounts():
-        """
-        get all accounts from Firefly-III
-        """
-        accounts = requests.get(
-            '{}/api/v1/accounts'.format(FF3_EXPORTER_BASEURL),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            accounts = accounts.json()
-            return accounts
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_accounts(): Response is not JSON format')
-
-    def ff3_accounts_transactions(account, start='%7B%7D', end='%7B%7D'):
-        """
-        get all account transactions
-        """
-        account_transactions = requests.get(
-            '{}/api/v1/accounts/{}/transactions?start={}&end={}'.format(
-                FF3_EXPORTER_BASEURL,
-                account,
-                start,
-                end),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            account_transactions = account_transactions.json()
-            return account_transactions
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_accounts_transactions(): Response is not JSON format')
-
-    def ff3_bills():
-        """
-        get list of bills
-        """
-        bills = requests.get(
-            '{}/api/v1/bills'.format(FF3_EXPORTER_BASEURL),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            bills = bills.json()
-            return bills
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_bills(): Response is not JSON format')
-
-    # TODO: get list of bills transactions /api/v1/bills/{id}/transactions
-    # def ff3_bills_transactions():
-    #     """
-    #     get transactions related to bills
-    #     """
-
-    def ff3_piggybanks():
-        """
-        get list of piggybanks
-        """
-        piggybanks = requests.get(
-            '{}/api/v1/piggy_banks'.format(FF3_EXPORTER_BASEURL),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            piggybanks = piggybanks.json()
-            return piggybanks
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_piggybanks(): Response is not JSON format')
-
-    def ff3_piggybanks_details(piggybank_id):
-        """
-        get piggybank details
-        """
-        piggybank_details = requests.get(
-            '{}/api/v1/piggy_banks/{}'.format(
-                FF3_EXPORTER_BASEURL,
-                piggybank_id),
-            headers=json.loads(FF3_EXPORTER_TOKEN))
-        try:
-            piggybank_details = piggybank_details.json()
-            return piggybank_details
-        except json.decoder.JSONDecodeError:
-            sys.exit('ff3_piggybanks_details(): Response is not JSON format')
-
-    start_http_server(FF3_EXPORTER_PORT)
-    REGISTRY.register(CustomCollector())
+    start_http_server(8000, 'localhost')
     while True:
+        collect()
         print('Checked API: {}'.format(datetime.now().strftime("%Y-%m-%d @ %H:%M:%S.%f")))
         time.sleep(FF3_EXPORTER_SLEEP)
